f4d18c6a594b0e4f16f68b6388c60cee1bfdc2a9
akileez <git@keeda.net>
2015-09-20 04:38:02 -0400

    install JS configs

---
+++ app/cfg/uncss.js
@@ -0,0 +1,14 @@
+module.exports = {
+  ignore: [
+    ".fade",
+    ".fade.in",
+    ".collapse",
+    ".collapse.in",
+    ".collapsing",
+    ".alert-danger",
+    ".navbar-collapse.in",
+    /\.open/
+  ],
+  ignoreSheets: [/fonts.googleapis/],
+  report: false
+}
